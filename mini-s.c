2a942f5a15ec1f81b7b8fc72f5a55bb3290f91cb
EngThi <thiago.teixeira51@etec.sp.gov.br>
2026-06-13 13:15:14 -0300

    feat: cd command

--- mini-s.c
+++ mini-s.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void salvar_historico(char *input) {
   FILE *file = fopen("history.txt", "a");
@@ -33,7 +34,7 @@ int main() {
     printf("> ");
     fgets(input, sizeof(input), stdin);
 
-    input[strcspn(input, "\n")] = 0;
+    input[strcspn(input, "\r\n")] = 0;
 
     salvar_historico(input);
 
@@ -45,27 +46,17 @@ int main() {
     if (strcmp(cmd, "exit") == 0) {
       printf("Saindo...\n");
       break;
-    }
+    } else if (strcmp(cmd, "cd") == 0) {
+      char *path = strtok(NULL, " ");
 
-    else if (strcmp(cmd, "run") == 0) {
-      char comando[100] = "";
-
-      char *token = strtok(NULL, " ");
-
-      while (token != NULL) {
-        strcat(comando, token);
-        strcat(comando, " ");
-        token = strtok(NULL, " ");
+      if (path == NULL) {
+        printf("Use: cd <diretorio>\n");
+      } else {
+        if (chdir(path) != 0) {
+          printf("Erro ao mudar diretório\n");
+        }
       }
-
-      system(comando);
-    }
-
-    else if (strcmp(cmd, "clear_history") == 0) {
-      limpar_historico();
-    }
-
-    else if (strcmp(cmd, "history") == 0) {
+    } else if (strcmp(cmd, "history") == 0) {
       FILE *file = fopen("history.txt", "r");
 
       if (file == NULL) {
@@ -74,12 +65,38 @@ int main() {
       }
 
       char linha[100];
+      int contador = 1;
 
       while (fgets(linha, sizeof(linha), file)) {
-        printf("%s", linha);
+        printf("%d: %s", contador, linha);
+        contador++;
       }
 
       fclose(file);
+    }
+
+    else if (strcmp(cmd, "clear_history") == 0) {
+      limpar_historico();
+    }
+
+    else if (strcmp(cmd, "run") == 0) {
+      char comando[100] = "";
+
+      char *token = strtok(NULL, " ");
+
+      if (token == NULL) {
+        printf("Use: run <comando>\n");
+        continue;
+      }
+
+      while (token != NULL) {
+        strcat(comando, token);
+        strcat(comando, " ");
+        token = strtok(NULL, " ");
+      }
+
+      printf("Executando: %s\n", comando);
+      system(comando);
     }
 
     // help
